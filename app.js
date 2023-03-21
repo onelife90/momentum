e038e4672d00bb2f2b48a9df9f620177eb1d7bed
booster <onelife90@naver.com>
2023-03-21 18:22:20 +0900

    Loading username

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
+const HIDDEN_USERNAME = "hidden";
+const USERNAME_KEY = "username";
+
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-const HIDDEN_USERNAME = "hidden";
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_USERNAME);
+}
 
 function onLoginSubmit(event) {
   const username = loginInput.value;
   event.preventDefault();
 
   loginForm.classList.add(HIDDEN_USERNAME);
-  localStorage.setItem("username", username);
+  localStorage.setItem(USERNAME_KEY, username);
 
-  greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_USERNAME);
+  paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+// check for username in localStorage
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_USERNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
